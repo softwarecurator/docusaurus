43d40c372fa6b178aeba10c40db6f4129b39674c
Joseph Rios <blankworldwide@gmail.com>
2021-01-25 12:17:59 -0800

    Default auto collapse to false

--- packages/docusaurus-theme-classic/src/theme/DocPage/index.tsx
+++ packages/docusaurus-theme-classic/src/theme/DocPage/index.tsx
@@ -85,6 +85,9 @@ function DocPageContent({
               sidebarCollapsible={
                 siteConfig.themeConfig?.sidebarCollapsible ?? true
               }
+              autoCollapseSidebar={
+                siteConfig.themeConfig?.autoCollapseSidebar ?? false
+              }
               onCollapse={toggleSidebar}
               isHidden={hiddenSidebar}
             />
